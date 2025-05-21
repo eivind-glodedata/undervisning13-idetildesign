98e7621b9ca09d1d43ae6b6ea3ddb2c68e787118
eivind-glodedata <eivind.glodedata@gmail.com>
2025-05-21 13:40:54 +0200

    ferdig undervisning

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 console.log('Hello!');
-function typeText(elementId, text, speed) {
-  let i = 0;
-  const element = document.getElementById(elementId);
+//function typeText(elementId, text, speed) {
+ // let i = 0;
+  //const element = document.getElementById(elementId);
 
-  function type() {
-    if (i < text.length) {
-      element.innerHTML += text.charAt(i);
-      i++;
-      setTimeout(type, speed);
-    }
-  }
+ // function type() {
+ //   if (i < text.length) {
+ //     element.innerHTML += text.charAt(i);
+ //     i++;
+ //     setTimeout(type, speed);
+ //   }
+ // }
 
-  type();
-}
+  //type();
+//}
 
-typeText(
+/*typeText(
   'print',
   `Arma virumque cano, Troiae qui primus ab oris Italiam, fato profugus, Laviniaque venit
 litora, multum ille et terris iactatus et alto vi superum saevae memorem Iunonis ob iram; multa quoque et bello
@@ -30,9 +30,49 @@ exciderant animo: manet alta mente repostum iudicium Paridis spretaeque iniuria 
 Ganymedis honores. His accensa super, iactatos aequore totoTroas, reliquias Danaum atque immitis Achilli, arcebat
 longe Latio, multosque per annos errabant, acti fatis, maria omnia circum. Tantae molis erat Romanam condere gentem!`,
   100
-);
+);*/
 
 //This will create a typing effect where "Hello, world!" is typed out in the element with ID myElement at a speed of 100 milliseconds per character.
 
 // Usage example:
 // typeText('myElement', 'Hello, world!', 100);
+
+//menu.html
+console.log("Working from Menu?");
+const pizzaCardContainer = document.querySelector("#pizzaCardContainer");
+console.log(pizzaCardContainer);
+
+const pizzaCard = document.createElement("div");
+pizzaCard.classList.add("pizzaCard");
+
+const pizzaCardImg = document.createElement("img");
+pizzaCardImg.classList.add("pizzaCardImg");
+
+pizzaCard.appendChild(pizzaCardImg);
+pizzaCardImg.src = "";
+pizzaCardImg.alt = "Bildet av pizzaen";
+
+const pizzaCardTitle = document.createElement("h3");
+pizzaCardTitle.classList.add("pizzaCardTitle");
+pizzaCardTitle.textContent = "Pizza navn";
+
+pizzaCard.appendChild(pizzaCardImg);
+pizzaCard.appendChild(pizzaCardTitle);
+//legg til pizzaCardDescription til pizzaCard
+//legg til pizzaCardPrize til pizzaCard
+
+const pizzaCardDescription = document.createElement("p");
+pizzaCardDescription.classList.add("pizzaCardDescription");
+pizzaCardDescription.textContent = "Pizzabeskrivelse";
+pizzaCard.appendChild(pizzaCardDescription);
+
+const pizzaCardPrize = document.createElement("p");
+pizzaCardPrize.classList.add("pizzaCardPrize");
+pizzaCardPrize.textContent = "Pizzapris";
+
+pizzaCard.appendChild(pizzaCardPrize);
+
+pizzaCardContainer.appendChild(pizzaCard.cloneNode(true));
+console.log(pizzaCardContainer);
+pizzaCardContainer.appendChild(pizzaCard.cloneNode(true));
+pizzaCardContainer.appendChild(pizzaCard.cloneNode(true));
